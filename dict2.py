1581c71ab63224b29ce35855ad52a81639825873
Юлия Трегубова <tregubova_julia@mail.ru>
2020-04-14 22:34:25 +0500

    первый коммит

---
+++ dict2.py
@@ -0,0 +1,18 @@
+#codeing: utf-8
+#Словарь
+
+model = {
+	'Electrolux_IPE6440KF':34999,
+	'Hansa_BHI68313':20199,
+	'Bosh_PUE611FB1E':35199
+	}
+
+list_d = list(model.items())
+
+#найти наименьшую цену
+minimum = list_d[0][1]
+for i in range(len(list_d)):
+	if list_d[i][1] < minimum:
+		minimum = list_d[i][1]
+print(minimum)
+
